751ab19860c998cf5e0675e554bd162ec0214e67
PVN Creations <PVN Creations@PVN_Creations>
2022-09-21 13:20:03 +0530

    changes done by engg-2

--- src/test/java/org/sdet40/practice/AnnotationPractice.java
+++ src/test/java/org/sdet40/practice/AnnotationPractice.java
@@ -5,7 +5,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class AnnotationPractice {
+public class AnnotationPractice 
+	{
 	@BeforeMethod
 	public void beforeTest()
 	{
